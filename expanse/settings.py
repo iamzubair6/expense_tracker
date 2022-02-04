ef6907085fcd0d8f22e87741cfd0e4e18cdeb950
Zubair Rahman <zubair.rahman89@gmail.com>
2022-02-05 00:15:03 +0600

    server file up

--- expanse/settings.py
+++ expanse/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,12 +76,28 @@ WSGI_APPLICATION = 'expanse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# heroku database postgre
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dlv2fjtmjp1ah',
+        'USER': 'tzyqsaejecjhzx',
+        'PASSWORD': '2f0a95176614bf29f7f74f50e1496287c1ed507a3ad7dcb1bf208a97080b9e09',
+        'HOST': 'ec2-3-225-79-57.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 
 
 # Password validation
